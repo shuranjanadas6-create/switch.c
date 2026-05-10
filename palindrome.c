74cdfba545b27371504ed873092e877a8ec7806d
Shuranjana Das <shuranjanadas6@example.com>
2026-05-10 12:22:11 +0530

    palindrome program complete

---
+++ palindrome.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, remainder, original;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    // Reverse the number
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    // Check palindrome
+    if (original == reversed)
+        printf("%d is a palindrome.\n", original);
+    else
+        printf("%d is not a palindrome.\n", original);
+
+    return 0;
+}
